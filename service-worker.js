25eefbb35a0857b34e19c2075f772ccf2c29d505
vitamaxdh <hellonewtry@gmail.com>
2024-02-07 19:43:55 -0500

    chore: remove console log

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,5 @@
 chrome.commands.onCommand.addListener(function (command) {
   chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-    console.log('hello');
     if (tabs.length > 0 && tabs[0].url.includes('youtube.com/watch')) {
       chrome.tabs.sendMessage(tabs[0].id, { command: command, tabId: tabs[0].id });
     }
