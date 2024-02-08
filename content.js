aae2a73196f190f0223cbce6b07f18ed530ead3e
vitamaxdh <hellonewtry@gmail.com>
2024-02-07 19:44:46 -0500

    fix: non responding to first execution

--- content.js
+++ content.js
@@ -1,34 +1,25 @@
 let initialized = false;
-async function injectScript(fileURL) {
-  new Promise((resolve, reject) => {
-    if (initialized) {
-      return;
-    }
-    const body = document.body;
+
+async function injectScript(filePath) {
+  if (initialized) {
+    return Promise.resolve();
+  }
+  return new Promise((resolve, reject) => {
     const script = document.createElement('script');
     script.setAttribute('type', 'text/javascript');
-    script.setAttribute('src', fileURL);
-
-    script.onload = resolve;
+    script.setAttribute('src', filePath);
+    script.onload = () => {
+      initialized = true;
+      resolve();
+    };
     script.onerror = reject;
-
-    body.appendChild(script);
-    initialized = true
+    document.body.appendChild(script);
   });
 }
 
-chrome.runtime.onMessage.addListener(
-  async (request, sender, sendResponse) => {
-    console.log('initialized', initialized);
-    console.log('request', request);
-    if (!initialized) {
-      await injectScript(chrome.runtime.getURL('./quality_control.js'));
-    }
-    let command = { command: request.command };
-    let event = new CustomEvent('qualityControlEvent', { detail: command });
-    console.log('command', command);
-    console.log('event', event);
-    document.dispatchEvent(event);
-  }
-);
-
+chrome.runtime.onMessage.addListener(async (request, sender, sendResponse) => {
+  await injectScript(chrome.runtime.getURL('./quality_control.js'));
+  let command = { command: request.command };
+  let event = new CustomEvent('qualityControlEvent', { detail: command });
+  document.dispatchEvent(event);
+});
