d93cca3c67a3096ecce99a88e7d752e3c913e064
vitamaxdh <hellonewtry@gmail.com>
2024-02-13 21:01:07 -0500

    fix: apply linux on popup.html

--- check_os.js
+++ check_os.js
@@ -1,13 +1,12 @@
 window.onload = function() {
-    var isMac = navigator.userAgent.indexOf('Mac OS X') != -1;
-    var windowsShortcuts = document.getElementById('windowsShortcuts');
-    var macShortcuts = document.getElementById('macShortcuts');
+    const isMac = navigator.userAgent.indexOf('Mac OS X') != -1;
+
+    const windowsShortcuts = document.getElementById('windowsShortcuts');
+    const macShortcuts = document.getElementById('macShortcuts');
 
     if (isMac) {
-        windowsShortcuts.style.display = 'none';
         macShortcuts.style.display = 'block';
     } else {
         windowsShortcuts.style.display = 'block';
-        macShortcuts.style.display = 'none';
     }
 };
